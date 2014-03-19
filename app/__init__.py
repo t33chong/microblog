bb084fb87145d42d801fcd251931c8a2ce6e29ab
Tristan Euan Chong <tristaneuan@gmail.com>
2014-03-18 19:16:15 -0700

    configure login and openid

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
+import os
 from flask import Flask
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
 from flask.ext.sqlalchemy import SQLAlchemy
+
+from app import models, views
+from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
+
 db = SQLAlchemy(app)
 
-from app import models, views
+lm = LoginManager()
+lm.init_app(app)
+
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
