edf9fe7c886ae4c1c13d57161f62ec3543db6eed
Tristan Euan Chong <tristaneuan@gmail.com>
2014-03-19 22:03:32 -0700

    add avatar method

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from hashlib import md5
+
 from app import db
 
 ROLE_USER = 0
@@ -10,6 +12,11 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     role = db.Column(db.SmallInteger, default=ROLE_USER)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def avatar(self, size):
+        return (
+            'http://www.gravatar.com/avatar/' + md5(self.email).hexdigest() +
+            '?d=mm&s=' + str(size))
 
     def is_authenticated(self):
         return True
