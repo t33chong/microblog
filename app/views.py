306dc70126975c1725021245a88b9295aac5d22a
Tristan Euan Chong <tristaneuan@gmail.com>
2014-03-19 20:12:53 -0700

    add logout view

--- app/views.py
+++ app/views.py
@@ -65,3 +65,9 @@ def after_login(resp):
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+
+@app.route('/logout/')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
