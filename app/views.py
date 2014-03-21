d55ef5c1ba3225eb3e2d228ec339a939f44c5582
Tristan Euan Chong <tristaneuan@gmail.com>
2014-03-20 20:26:20 -0700

    commit last_seen to db before request

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
@@ -26,6 +27,10 @@ def index():
 @app.before_request
 def before_request():
     g.user = current_user
+    if g.user.is_authenticated():
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
 
 
 @app.route('/login/', methods=['GET', 'POST'])
