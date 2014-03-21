96a52e2767c713e5663ba3277e9f92831d789632
Tristan Euan Chong <tristaneuan@gmail.com>
2014-03-20 20:44:37 -0700

    add EditForm class

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,13 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField
-from wtforms.validators import Required
+from wtforms import BooleanField, TextAreaField, TextField
+from wtforms.validators import Length, Required
 
 
 class LoginForm(Form):
     openid = TextField('openid', validators=[Required()])
     remember_me = BooleanField('remember_me', default=False)
+
+
+class EditForm(Form):
+    nickname = TextField('nickname', validators=[Required()])
+    about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
