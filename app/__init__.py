4213d3f96a6c24e9a5184719d3d2b9f04d7acd6a
Tristan Euan Chong <tristaneuan@gmail.com>
2014-03-24 13:41:05 -0600

    add mail logging handler

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask.ext.openid import OpenID
 from flask.ext.sqlalchemy import SQLAlchemy
 
 from config import basedir
+from config import ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -16,5 +17,17 @@ lm.init_app(app)
 lm.login_view = 'login'
 
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+if not app.debug:
+    import logging
+    from logging.handlers import SMTPHandler
+    credentials = None
+    if MAIL_USERNAME or MAIL_PASSWORD:
+        credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+    mail_handler = SMTPHandler(
+        (MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS,
+        'microblog failure', credentials)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
 
 from app import models, views
