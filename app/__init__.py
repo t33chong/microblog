5553ca3b6895e6832245d6f3c46fc163f504fe32
Tristan Euan Chong <tristaneuan@gmail.com>
2014-03-24 15:12:33 -0600

    add file logging

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,11 @@ oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 if not app.debug:
     import logging
-    from logging.handlers import SMTPHandler
+    from logging.handlers import RotatingFileHandler, SMTPHandler
+
+    app.logger.setLevel(logging.INFO)
+
+    # Email logging
     credentials = None
     if MAIL_USERNAME or MAIL_PASSWORD:
         credentials = (MAIL_USERNAME, MAIL_PASSWORD)
@@ -29,5 +33,17 @@ if not app.debug:
         credentials)
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
+
+    # File logging
+    file_handler = RotatingFileHandler(
+        'tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(
+        logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+            )
+        )
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('microblog startup')
 
 from app import models, views
