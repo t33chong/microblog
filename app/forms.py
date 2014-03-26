ad39a8c6129d6f3f34a0a964508ed5b23e8b3d51
Tristan Euan Chong <tristaneuan@gmail.com>
2014-03-26 11:29:29 -0600

    validate that nickname is not already in use

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,8 @@ from flask.ext.wtf import Form
 from wtforms import BooleanField, TextAreaField, TextField
 from wtforms.validators import Length, Required
 
+from app.models import User
+
 
 class LoginForm(Form):
     openid = TextField('openid', validators=[Required()])
@@ -11,3 +13,19 @@ class LoginForm(Form):
 class EditForm(Form):
     nickname = TextField('nickname', validators=[Required()])
     about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
+
+    def __init__(self, original_nickname, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+        self.original_nickname = original_nickname
+
+    def validate(self):
+        if not Form.validate(self):
+            return False
+        if self.nickname.data == self.original_nickname:
+            return True
+        user = User.query.filter_by(nickname=self.nickname.data).first()
+        if user is not None:
+            self.nickname.errors.append(
+                'This nickname is already in use. Please choose another one.')
+            return False
+        return True
