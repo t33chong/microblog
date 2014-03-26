d72427ddae3501c1221481b7f99f62da7f573266
Tristan Euan Chong <tristaneuan@gmail.com>
2014-03-26 14:37:29 -0600

    add TLS support to SMTPHandler for Gmail

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,8 @@ oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 if not app.debug:
     import logging
-    from logging.handlers import RotatingFileHandler, SMTPHandler
+    from handlers import TlsSMTPHandler
+    from logging.handlers import RotatingFileHandler
 
     app.logger.setLevel(logging.INFO)
 
@@ -28,8 +29,8 @@ if not app.debug:
     credentials = None
     if MAIL_USERNAME or MAIL_PASSWORD:
         credentials = (MAIL_USERNAME, MAIL_PASSWORD)
-    mail_handler = SMTPHandler(
-        (MAIL_SERVER, MAIL_PORT), ADMINS[0], ADMINS, 'microblog failure',
+    mail_handler = TlsSMTPHandler(
+        (MAIL_SERVER, MAIL_PORT), MAIL_USERNAME, ADMINS, 'microblog failure',
         credentials)
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
