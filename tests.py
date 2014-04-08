6aa259a0c499426a5fcb5e3493d303a0b26d41ef
Tristan Chong <tristanchong@gmail.com>
2014-04-07 22:05:06 -0700

    add test_followed_posts()

--- tests.py
+++ tests.py
@@ -1,8 +1,9 @@
 import os
 import unittest
+from datetime import datetime, timedelta
 
 from app import app, db
-from app.models import User
+from app.models import Post, User
 from config import basedir
 
 
@@ -63,6 +64,62 @@ class TestCase(unittest.TestCase):
         assert u1.followed.count() == 0
         assert u2.followers.count() == 0
 
+    def test_followed_posts(self):
+        # Make four users
+        u1 = User(nickname='john', email='john@example.com')
+        u2 = User(nickname='susan', email='susan@example.com')
+        u3 = User(nickname='mary', email='mary@example.com')
+        u4 = User(nickname='david', email='david@example.com')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.add(u3)
+        db.session.add(u4)
+
+        # Make four posts
+        utcnow = datetime.utcnow()
+        p1 = Post(body='post from john', author=u1,
+                  timestamp=utcnow+timedelta(seconds=1))
+        p2 = Post(body='post from susan', author=u2,
+                  timestamp=utcnow+timedelta(seconds=2))
+        p3 = Post(body='post from mary', author=u3,
+                  timestamp=utcnow+timedelta(seconds=3))
+        p4 = Post(body='post from david', author=u4,
+                  timestamp=utcnow+timedelta(seconds=4))
+        db.session.add(p1)
+        db.session.add(p2)
+        db.session.add(p3)
+        db.session.add(p4)
+        db.session.commit()
+
+        # Set up the followers
+        u1.follow(u1)  # John follows himself
+        u1.follow(u2)  # John follows Susan
+        u1.follow(u4)  # John follows David
+        u2.follow(u2)  # Susan follows herself
+        u2.follow(u3)  # Susan follows Mary
+        u3.follow(u3)  # Mary follows herself
+        u3.follow(u4)  # Mary follows David
+        u4.follow(u4)  # David follows himself
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.add(u3)
+        db.session.add(u4)
+        db.session.commit()
+
+        # Check the followed posts of each user
+        f1 = u1.followed_posts().all()
+        f2 = u2.followed_posts().all()
+        f3 = u3.followed_posts().all()
+        f4 = u4.followed_posts().all()
+
+        assert len(f1) == 3
+        assert len(f2) == 2
+        assert len(f3) == 2
+        assert len(f4) == 1
+        assert f1 == [p4, p2, p1]
+        assert f2 == [p3, p2]
+        assert f3 == [p4, p3]
+        assert f4 == [p4]
 
 
 if __name__ == '__main__':
