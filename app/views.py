573155602dacd83a9bdca76bd5d6ae9b853c0e58
Tristan Chong <tristanchong@gmail.com>
2014-04-15 22:20:40 -0700

    paginate user view

--- app/views.py
+++ app/views.py
@@ -84,16 +84,14 @@ def logout():
 
 
 @app.route('/user/<nickname>/')
+@app.route('/user/<nickname>/<int:page>/')
 @login_required
-def user(nickname):
+def user(nickname, page=1):
     user = User.query.filter_by(nickname=nickname).first()
     if user is None:
         flash('User ' + nickname + ' not found.')
         return redirect(url_for('index'))
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-        ]
+    posts = user.posts.paginate(page, POSTS_PER_PAGE, False)
     return render_template('user.html', user=user, posts=posts)
 
 
