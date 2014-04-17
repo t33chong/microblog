31e4258595970f0bbc052806235811264e45df9e
Tristan Chong <tristanchong@gmail.com>
2014-04-16 22:56:01 -0700

    use async decorator

--- app/emails.py
+++ app/emails.py
@@ -1,11 +1,12 @@
 from flask import render_template
 from flask.ext.mail import Message
-from threading import Thread
 
 from app import mail
 from config import ADMINS
+from decorators import async
 
 
+@async
 def send_async_email(msg):
     mail.send(msg)
 
@@ -14,8 +15,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    thr = Thread(target=send_async_email, args=[msg])
-    thr.start()
+    send_async_email(msg)
 
 
 def follower_notification(followed, follower):
