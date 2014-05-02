b0e04d76fb580611dce73ba98535c67932574fab
Tristan Chong <tristanchong@gmail.com>
2014-05-01 20:57:28 -0700

    wrap strings in babel gettext

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.babel import gettext
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, babel, db, lm, oid
 from config import LANGUAGES, MAX_SEARCH_RESULTS, POSTS_PER_PAGE
@@ -19,7 +20,7 @@ def index(page=1):
             body=form.post.data, timestamp=datetime.utcnow(), author=g.user)
         db.session.add(post)
         db.session.commit()
-        flash('Your post is now live!')
+        flash(gettext('Your post is now live!'))
         # Redirect to avoid form resubmission upon browser refresh
         return redirect(url_for('index'))
     posts = g.user.followed_posts().paginate(page, POSTS_PER_PAGE, False)
@@ -45,14 +46,14 @@ def login():
     if form.validate_on_submit():
         session['remember_me'] = form.remember_me.data
         return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
-    return render_template('login.html', title='Sign In', form=form,
+    return render_template('login.html', title=gettext('Sign In'), form=form,
                            providers=app.config['OPENID_PROVIDERS'])
 
 
 @oid.after_login
 def after_login(resp):
     if resp.email is None or resp.email == '':
-        flash('Invalid login. Please try again.')
+        flash(gettext('Invalid login. Please try again.'))
         return redirect(url_for('login'))
     user = User.query.filter_by(email=resp.email).first()
     if user is None:
@@ -91,7 +92,7 @@ def logout():
 def user(nickname, page=1):
     user = User.query.filter_by(nickname=nickname).first()
     if user is None:
-        flash('User ' + nickname + ' not found.')
+        flash(gettext('User ' + nickname + ' not found.'))
         return redirect(url_for('index'))
     posts = user.posts.paginate(page, POSTS_PER_PAGE, False)
     return render_template('user.html', user=user, posts=posts)
@@ -106,7 +107,7 @@ def edit():
         g.user.about_me = form.about_me.data
         db.session.add(g.user)
         db.session.commit()
-        flash('Your changes have been saved.')
+        flash(gettext('Your changes have been saved.'))
         return redirect(url_for('edit'))
     else:
         form.nickname.data = g.user.nickname
@@ -119,18 +120,18 @@ def edit():
 def follow(nickname):
     user = User.query.filter_by(nickname=nickname).first()
     if user is None:
-        flash('User ' + nickname + ' not found.')
+        flash(gettext('User ' + nickname + ' not found.'))
         return redirect(url_for('index'))
     if user == g.user:
-        flash("You can't follow yourself!")
+        flash(gettext('You can't follow yourself!'))
         return redirect(url_for('user', nickname=nickname))
     u = g.user.follow(user)
     if u is None:
-        flash('Cannot follow ' + nickname + '.')
+        flash(gettext('Cannot follow ' + nickname + '.'))
         return redirect(url_for('user', nickname=nickname))
     db.session.add(u)
     db.session.commit()
-    flash('You are now following ' + nickname + '!')
+    flash(gettext('You are now following ' + nickname + '!'))
     follower_notification(user, g.user)
     return redirect(url_for('user', nickname=nickname))
 
@@ -140,18 +141,18 @@ def follow(nickname):
 def unfollow(nickname):
     user = User.query.filter_by(nickname=nickname).first()
     if user is None:
-        flash('User ' + nickname + ' not found.')
+        flash(gettext('User ' + nickname + ' not found.'))
         return redirect(url_for('index'))
     if user == g.user:
-        flash("You can't unfollow yourself!")
+        flash(gettext('You can't unfollow yourself!'))
         return redirect(url_for('user', nickname=nickname))
     u = g.user.unfollow(user)
     if u is None:
-        flash('Cannot unfollow ' + nickname + '.')
+        flash(gettext('Cannot unfollow ' + nickname + '.'))
         return redirect(url_for('user', nickname=nickname))
     db.session.add(u)
     db.session.commit()
-    flash('You have stopped following ' + nickname + '.')
+    flash(gettext('You have stopped following ' + nickname + '.'))
     return redirect(url_for('user', nickname=nickname))
 
 
