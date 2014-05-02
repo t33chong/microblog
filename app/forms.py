243c0b20eb95165d9b382b63e28c393fc7d392b1
Tristan Chong <tristanchong@gmail.com>
2014-05-01 21:46:21 -0700

    check that nickname is valid in EditForm

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,4 @@
+from flask.ext.babel import gettext
 from flask.ext.wtf import Form
 from wtforms import BooleanField, TextAreaField, TextField
 from wtforms.validators import Length, Required
@@ -23,6 +24,11 @@ class EditForm(Form):
             return False
         if self.nickname.data == self.original_nickname:
             return True
+        if self.nickname.data != User.make_valid_nickname(self.nickname.data):
+            self.nickname.errors.append(gettext(
+                'This nickname has invalid characters. ' +
+                'Please use letters, numbers, dots, and underscores only.'))
+            return False
         user = User.query.filter_by(nickname=self.nickname.data).first()
         if user is not None:
             self.nickname.errors.append(
